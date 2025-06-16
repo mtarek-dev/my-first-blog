ecbdd62675d034369e2ae41dd3d3e977a2dc0c81
Mahmoud Tarek <mt@example.com>
2025-06-16 21:28:02 +0300

    new update

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth import views
 from django.urls import path, include
+from blog import views as v
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', v.logout_view, name='logout'),
     path('', include('blog.urls')),
 ]
